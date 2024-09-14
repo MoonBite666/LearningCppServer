b5d31e0f01de1692f4d2e69c34b720d184746111
moonfeather <moonbite233@gmail.com>
2024-09-14 23:47:51 +0800

    add echo server

--- EchoSocket/Client.cpp
+++ EchoSocket/Client.cpp
@@ -9,7 +9,7 @@ Client::Client() {
     bzero(&_addr,sizeof(_addr));
     _addr.sin_family = AF_INET;
     _addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    _addr.sin_port = htons(9090);
+    _addr.sin_port = htons(9091);
 }
 
 Client::~Client() {
@@ -19,6 +19,26 @@ Client::~Client() {
 void Client::Connect() {
     if(int err = connect(_sockfd, reinterpret_cast<sockaddr*>(&_addr), sizeof(_addr))) {
         perror("client error: ");
+    }
+    char buf[1024];
+    while(true) {
+        bzero(&buf,sizeof(buf));
+
+        if(ssize_t write_bytes = write(_sockfd,buf,sizeof(buf)); write_bytes == -1){
+            printf("socket already disconnected, can't write any more!\n");
+            break;
+        }
+        bzero(buf, sizeof(buf));
+        ssize_t read_bytes = read(_sockfd, buf, sizeof(buf));
+        if(read_bytes > 0){
+            printf("message from server: %s\n", buf);
+        }else if(read_bytes == 0){
+            printf("server socket disconnected!\n");
+            break;
+        }else if(read_bytes == -1){
+            close(_sockfd);
+            perror("socket read error");
+        }
     }
 }
 
--- EchoSocket/Server.cpp
+++ EchoSocket/Server.cpp
@@ -23,7 +23,7 @@ Server::Server() {
 
     _addr.sin_family = AF_INET;
     _addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    _addr.sin_port = htons(9090);
+    _addr.sin_port = htons(9091);
 
     if(int err = bind(_sockfd, reinterpret_cast<sockaddr*>(&_addr), sizeof(_addr))) {
         perror("Socket init failed: ");
@@ -37,13 +37,31 @@ Server::~Server() {
 }
 
 void Server::waitConnection() const {
-    printf("Waiting for Client......");
+    printf("Waiting for Client......\n");
     sockaddr_in client_addr{};
     socklen_t addr_len = sizeof(client_addr);
     bzero(&client_addr, sizeof(client_addr));
 
     const int client_sockfd = accept(_sockfd, reinterpret_cast<sockaddr*>(&client_addr), &addr_len);
     printf("new client fd %d! IP: %s Port: %d\n", client_sockfd, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    char buf[1024];
+    while(true) {
+        bzero(&buf,sizeof(buf));
+        scanf("%s", buf);
+
+        if(ssize_t read_bytes = read(client_sockfd, buf, sizeof(buf)); read_bytes > 0) {
+            printf("message from client fd %d: %s\n", client_sockfd, buf);
+            write(client_sockfd, buf, sizeof(buf));
+        }
+        else if (read_bytes == 0) {
+            printf("client fd %d disconnected\n", client_sockfd);
+            break;
+        }
+        else {
+            perror("socket error:");
+            break;
+        }
+    }
 }
 
 
