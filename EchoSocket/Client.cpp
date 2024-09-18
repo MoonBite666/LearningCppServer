ec4f5f461b768bfa395065bc5f541d41478c6679
MoonFeather <moonbite233@gmail.com>
2024-09-18 09:54:58 +0800

    finish echo server

--- EchoSocket/Client.cpp
+++ EchoSocket/Client.cpp
@@ -9,7 +9,7 @@ Client::Client() {
     bzero(&_addr,sizeof(_addr));
     _addr.sin_family = AF_INET;
     _addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    _addr.sin_port = htons(9090);
+    _addr.sin_port = htons(9092);
 }
 
 Client::~Client() {
@@ -20,6 +20,29 @@ void Client::Connect() {
     if(int err = connect(_sockfd, reinterpret_cast<sockaddr*>(&_addr), sizeof(_addr))) {
         perror("client error: ");
     }
+
+    while(true){
+        char buf[1024];     //定义缓冲区
+        bzero(&buf, sizeof(buf));       //清空缓冲区
+        scanf("%s", buf);             //从键盘输入要传到服务器的数据
+        ssize_t write_bytes = write(_sockfd, buf, sizeof(buf));      //发送缓冲区中的数据到服务器socket，返回已发送数据大小
+        if(write_bytes == -1){          //write返回-1，表示发生错误
+            printf("socket already disconnected, can't write any more!\n");
+            break;
+        }
+        bzero(&buf, sizeof(buf));       //清空缓冲区
+        ssize_t read_bytes = read(_sockfd, buf, sizeof(buf));    //从服务器socket读到缓冲区，返回已读数据大小
+        if(read_bytes > 0){
+            printf("message from server: %s\n", buf);
+        }else if(read_bytes == 0){      //read返回0，表示EOF，通常是服务器断开链接，等会儿进行测试
+            printf("server socket disconnected!\n");
+            break;
+        }else if(read_bytes == -1){     //read返回-1，表示发生错误，按照上文方法进行错误处理
+            close(_sockfd);
+            perror("socket error:");
+        }
+    }
+
 }
 
 int main() {
--- EchoSocket/Server.cpp
+++ EchoSocket/Server.cpp
@@ -7,23 +7,13 @@
 #include <cstring>
 #include <unistd.h>
 
-Server* server = nullptr;
-
-void signalHandler(int signum) {
-    if(server) {
-        delete server;
-        server = nullptr;
-    }
-    exit(signum);
-}
-
 Server::Server() {
     _sockfd = socket(AF_INET, SOCK_STREAM, 0);
     bzero(&_addr,sizeof(_addr));
 
     _addr.sin_family = AF_INET;
     _addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    _addr.sin_port = htons(9090);
+    _addr.sin_port = htons(9092);
 
     if(int err = bind(_sockfd, reinterpret_cast<sockaddr*>(&_addr), sizeof(_addr))) {
         perror("Socket init failed: ");
@@ -44,12 +34,28 @@ void Server::waitConnection() const {
 
     const int client_sockfd = accept(_sockfd, reinterpret_cast<sockaddr*>(&client_addr), &addr_len);
     printf("new client fd %d! IP: %s Port: %d\n", client_sockfd, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+    while (true) {
+        char buf[1024];     //定义缓冲区
+        bzero(&buf, sizeof(buf));       //清空缓冲区
+        ssize_t read_bytes = read(client_sockfd, buf, sizeof(buf)); //从客户端socket读到缓冲区，返回已读数据大小
+        if(read_bytes > 0){
+            printf("message from client fd %d: %s\n", client_sockfd, buf);
+            write(client_sockfd, buf, sizeof(buf));           //将相同的数据写回到客户端
+        } else if(read_bytes == 0){             //read返回0，表示EOF
+            printf("client fd %d disconnected\n", client_sockfd);
+            close(client_sockfd);
+            break;
+        } else if(read_bytes == -1){        //read返回-1，表示发生错误，按照上文方法进行错误处理
+            close(client_sockfd);
+            perror("socket error:");
+        }
+    }
 }
 
 
 int main() {
-    signal(SIGINT, signalHandler);
-    server = new Server();
+    auto server = new Server();
     server->waitConnection();
     delete server;
     return 0;
